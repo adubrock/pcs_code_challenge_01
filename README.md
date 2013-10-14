66ebada832a77ec47fa33e2cd0f506a48d44ba46
Andrew DuBrock <adubrock@comcast.net>
2013-10-14 00:05:15 -0700

    increment 5

--- README.md
+++ README.md
@@ -3,23 +3,26 @@ pcs_code_challenge_01
 
 Growing Madlibs Code Challenge
 
-Increment 4
+Increment 5
+** New features outlined in 1b and 3.
 
 
 1. Uses a class to:
 
-	a. Gather the following parts of speech from the user (each can be a word 	or phrase):
+	a. Gather the following parts of speech from the user (each can be a word or phrase):
 		Noun
 		Adjective
 		Verb
 		Adverb
 		Object
 
-	b. Store all of these in a hash where the key is the name of the part of speech. For example, "noun" or "verb".
+	b. (New Feature) Gather a prepositional phrase via a multiple choice prompt. (If an improper number is detected, the user gets a pre-determined prepositional phrase).
+
+	b. Store all of these in a hash where the key is the name of the part of speech. For example, "noun," "verb," or "prep_phrase."
 
 2. Stores the hash values outside the class
 
-3. Creates an array with three sentence templates. 
+3. Creates an array with four sentence templates. New: one more template added in this increment, which includes the new prepositional phrase, and one of the older templates now includes this prepositional phrase.
 
 4. Uses another class to insert the hash values into the sentence templates
 
--- madlibs.rb
+++ madlibs.rb
@@ -1,4 +1,4 @@
-# This is increment 4
+# This is increment 5
 
 class Madlibs
 	def self.prompt
@@ -17,12 +17,36 @@ class Madlibs
 		puts "Please enter an object or object phrase:"
 		object = gets.strip.downcase
 
+		puts "Choose one of the following:
+
+		1. In a boat
+		2. On a bus
+		3. Inside a whale
+		4. Clinging under a speeding truck
+
+		by typing the number of your choice:"
+		prep_phrase = gets.strip
+		if prep_phrase == "1"
+			prep_phrase = "in a boat"
+		elsif prep_phrase == "2"
+			prep_phrase = "on a bus"
+		elsif prep_phrase == "3"
+			prep_phrase = "inside a whale"
+		elsif prep_phrase == "4"
+			prep_phrase = "clinging under a speeding truck"
+		else
+			puts "You did not choose a valid number! By default, your phrase is now 'behind the PCS dinosaur.'
+			"
+			prep_phrase = "behind the PCS dinosaur"
+		end
+
 		madlibs = {
 			"noun" => noun, 
 			"adjective" => adjective, 
 			"verb" => verb, 
 			"adverb" => adverb, 
-			"object" => object
+			"object" => object,
+			"prep_phrase" => prep_phrase
 		}
 	end
 end
@@ -32,7 +56,8 @@ madlibs = Madlibs.prompt
 template_array = [
 			"The #{madlibs['adjective']} #{madlibs['noun']} suddenly and #{madlibs['adverb']} had to #{madlibs['verb']} the #{madlibs['object']}", 
 			"The #{madlibs['noun']} jumped over the #{madlibs['object']}", 
-			"The #{madlibs['noun']} says 'let's #{madlibs['adverb']} #{madlibs['verb']} to the #{madlibs['adjective']} #{madlibs['object']}!'"
+			"The #{madlibs['noun']} says 'let's #{madlibs['adverb']} #{madlibs['verb']} #{madlibs['prep_phrase']} to the #{madlibs['adjective']} #{madlibs['object']}!'",
+			"He looked at the #{madlibs['noun']} and said 'Hey! Did you see that #{madlibs['object']} #{madlibs['prep_phrase']}? #{madlibs['verb'].upcase}! #{madlibs['verb'].upcase}!" 
 		]
 
 class SentenceTemplate
